e15a7aff24e712522f7c924d2709157095ac28e2
nick adams <thebigdeal916@icloud.com>
2020-06-30 16:12:32 -0700

    Updates

---
+++ precache-manifest.18a3ec2d60792501c41e91f0301e2f66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d4fbd8fe6bc78cf3dd42dfc3b193659",
+    "url": "/Cannabis-Site-Demo/index.html"
+  },
+  {
+    "revision": "117f30fa4a7300a578cf",
+    "url": "/Cannabis-Site-Demo/static/css/main.023e9aea.chunk.css"
+  },
+  {
+    "revision": "896aea8b39872566a4e6",
+    "url": "/Cannabis-Site-Demo/static/js/2.43615bc0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Cannabis-Site-Demo/static/js/2.43615bc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117f30fa4a7300a578cf",
+    "url": "/Cannabis-Site-Demo/static/js/main.a9853446.chunk.js"
+  },
+  {
+    "revision": "e71dbffe309946b8a2b3",
+    "url": "/Cannabis-Site-Demo/static/js/runtime-main.ca53effa.js"
+  }
+]);
