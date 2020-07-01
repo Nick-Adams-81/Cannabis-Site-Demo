f0854f1de9e9b197a3de79f7d3022fdc6d3fc367
nick adams <thebigdeal916@icloud.com>
2020-07-01 14:24:19 -0700

    color update

--- src/Cpmopnents/TopBrands/index.js
+++ src/Cpmopnents/TopBrands/index.js
@@ -4,7 +4,7 @@ import './style.css';
 
 function TopBrands() {
     return (
-        <div className='main'>
+        <div className='main-2'>
             <h2>Top Brands</h2>
             <div className="top-1">
                 <img src='https://www.designhill.com/resize_img.php?atyp=page_file&pth=ft_ca_ct||135||contestfile_4&flp=1553079080-12330718195c921b28e98836-44270820.png' height='140' width='140'></img>
